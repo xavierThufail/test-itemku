86dd75163f7506dc829d734fcf75b19d475d26a0
xavierThufail <xavier.thufail@gmail.com>
2020-06-04 17:41:48 +0700

    change Submit Question 3

--- question3.js
+++ question3.js
@@ -54,10 +54,9 @@ function solution(relation) {
       }
     }
     // check minimality
-    console.log(attributes)
     searchUnique(compare)
   }
   return answer;
 }
 
-console.log(solution([['100','ryan','music','2'],['200','apeach','math','2'],['300','tube','computer','3'],['400','con','computer','4'],['500','muzi','music','3'],['600','apeach','music','2']]))
+console.log(solution([['100','ryan','music','2'],['200','apeach','math','3'],['300','tube','computer','3'],['400','con','computer','4'],['500','muzi','music','3'],['600','apeach','music','2']]))
